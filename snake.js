bca50906696d54fa1d5577398d80ce697fc4b291
NIRZONE <nazisad15-2312@diu.edu.bd>
2025-06-04 01:38:37 +0600

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
-const box = 60;
+const box = 20;
 let direction = 'RIGHT';
 let running = false;
 let paused = false;
